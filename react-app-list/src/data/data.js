8ec038b969323fd0612614424032e364947ffe5d
henry <henry5720@hotmail.com.tw>
2024-06-25 21:45:46 +0800

    [Fix] change directory name

---
+++ react-app-list/src/data/data.js
@@ -0,0 +1,179 @@
+const data=[
+    {
+        style: "circle",
+        title: {
+            icon: "./assets/icon/fire.png",
+            text: "熱門角色一覽"
+        },
+        element: [
+            {
+                name: "01",
+                img: "./assets/01.jpg"
+            },
+            {
+                name: "02",
+                img: "./assets/02.jpg"
+            },
+            {
+                name: "03",
+                img: "./assets/03.jpg"
+            },
+            {
+                name: "04",
+                img: "./assets/04.jpg"
+            },
+            {
+                name: "05",
+                img: "./assets/05.jpg"
+            },
+        ]
+    },
+    {
+        style: "square",
+        title: {
+            icon: "./assets/icon/rank.png",
+            text: "熱播排行榜"
+        },
+        element: [
+            {
+                name: "01",
+                img: "./assets/01.jpg"
+            },
+            {
+                name: "02",
+                img: "./assets/02.jpg"
+            },
+            {
+                name: "03",
+                img: "./assets/03.jpg"
+            },
+            {
+                name: "04",
+                img: "./assets/04.jpg"
+            },
+            {
+                name: "05",
+                img: "./assets/05.jpg"
+            },
+        ]
+    },
+    {
+        style: "square",
+        title: {
+            icon: "./assets/icon/water.png",
+            text: "稀有角色"
+            },
+        element: [
+            {
+                name: "01",
+                img: "./assets/01.jpg"
+            },
+            {
+                name: "02",
+                img: "./assets/02.jpg"
+            },
+            {
+                name: "03",
+                img: "./assets/03.jpg"
+            },
+            {
+                name: "04",
+                img: "./assets/04.jpg"
+            },
+            {
+                name: "05",
+                img: "./assets/05.jpg"
+            },
+        ]
+    },
+    {
+        style: "square",
+        title: {
+            icon: "./assets/icon/water.png",
+            text: "一般角色"
+        },
+        element: [
+            {
+                name: "01",
+                img: "./assets/01.jpg"
+            },
+            {
+                name: "02",
+                img: "./assets/02.jpg"
+            },
+            {
+                name: "03",
+                img: "./assets/03.jpg"
+            },
+            {
+                name: "04",
+                img: "./assets/04.jpg"
+            },
+            {
+                name: "05",
+                img: "./assets/05.jpg"
+            },
+        ]
+    },
+    {
+        style: "square",
+        title: {
+            icon: "./assets/icon/water.png",
+            text: "特殊角色"
+        },
+        element: [
+            {
+                name: "01",
+                img: "./assets/01.jpg"
+            },
+            {
+                name: "02",
+                img: "./assets/02.jpg"
+            },
+            {
+                name: "03",
+                img: "./assets/03.jpg"
+            },
+            {
+                name: "04",
+                img: "./assets/04.jpg"
+            },
+            {
+                name: "05",
+                img: "./assets/05.jpg"
+            },
+        ]
+    },
+    {
+        style: "square",
+        title: {
+            icon: "./assets/icon/water.png",
+            text: "強力角色"
+        },
+        element: [
+            {
+                name: "01",
+                img: "./assets/01.jpg"
+            },
+            {
+                name: "02",
+                img: "./assets/02.jpg"
+            },
+            {
+                name: "03",
+                img: "./assets/03.jpg"
+            },
+            {
+                name: "04",
+                img: "./assets/04.jpg"
+            },
+            {
+                name: "05",
+                img: "./assets/05.jpg"
+            },
+        ]
+    },
+
+];
+
+export default data;
